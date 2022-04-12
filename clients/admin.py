7604071ee7161cff17fe0e2ea908df8c43efec8c
Boris Dibon <boris.dibon@gmail.com>
2022-04-12 14:20:58 +0200

    Add clients models and admin

--- clients/admin.py
+++ clients/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Client, Company
+from employees.models import Employee
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ("full_name", "company", "sales_contact", "is_prospect")
+    list_filter = ("is_prospect",)
+    search_fields = ("company__name", "sales_contact__email")
+
+    def has_change_permission(self, request, obj=None):
+        if request.user.role == Employee.MANAGER:
+            return True
+
+        if request.user.role == Employee.SALES_PERSON:
+            if obj is None:
+                return False
+            if obj.sales_contact == request.user:
+                return True
+            return False
+
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return self.has_change_permission(request, obj)
+
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
--- clients/models.py
+++ clients/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+from employees.models import Employee
+
+
+class Company(models.Model):
+    name = models.CharField(max_length=140)
+
+    class Meta:
+        verbose_name_plural = "companies"
+
+    def __str__(self):
+        return self.name
+
+
+class Client(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField(blank=True)
+    phone = PhoneNumberField(blank=True)
+    mobile = PhoneNumberField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_prospect = models.BooleanField(default=True)
+
+    sales_contact = models.ForeignKey(
+        Employee,
+        on_delete=models.PROTECT,
+        limit_choices_to={"role": Employee.SALES_PERSON},
+    )
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+
+    class Meta:
+        constraints = [
+            models.CheckConstraint(
+                check=models.Q(email__isnull=False)
+                | models.Q(phone__isnull=False)
+                | models.Q(mobile__isnull=False),
+                name="is_reachable",
+            ),
+        ]
+
+    def __str__(self):
+        return self.full_name
+
+    @property
+    def full_name(self):
+        return self.first_name + " " + self.last_name
