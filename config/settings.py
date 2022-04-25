d320d9857556d7bbc560bc72bcfb6d8586dc411f
Boris Dibon <boris.dibon@gmail.com>
2022-04-25 13:50:06 +0200

    Add back default authentication classes for the browsable API

--- config/settings.py
+++ config/settings.py
@@ -147,6 +147,8 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework_simplejwt.authentication.JWTAuthentication",
+        "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
     ],
 }
 
